7d64d9974bb5b4651b0881fbac72cc0945bdd453
Piyapong Thongman <65160230@go.buu.ac.th>
2024-08-05 23:27:34 +0700

    create file and add gitignore

---
+++ src/main/java/com/mycompany/lab_6_xo_file/Lab_6_XO_File.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lab_6_xo_file;
+
+/**
+ *
+ * @author tsuna
+ */
+public class Lab_6_XO_File {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
